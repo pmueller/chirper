f9553e088dd544bc5618afb5e9c385f6f353ad64
Preston Mueller <preston.mueller@techshido.com>
2012-02-20 00:58:07 -0500

    adding more actions

--- Shitter.rb
+++ Shitter.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
+  # redirect to login if not already logged in
+  redirect to '/login'
   haml :index
 end
 
@@ -9,10 +11,34 @@ get '/login' do
   haml :login
 end
 
+post '/login' do
+  # log that bitch in
+end
+
 get '/register' do
   haml :register
 end
 
+post 'register' do
+  #registerrrr the userrrrr
+end
+
 get '/search' do
   haml :search
+end
+
+post '/users' do
+  # make a user
+end
+
+get '/users/:id' do
+  # get the user's info... only for that user
+end
+
+put '/users/:id' do
+  # edit user info
+end
+
+post '/sheets' do
+  # someone made a sheet
 end
