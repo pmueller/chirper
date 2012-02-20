55fda1fc50c0203003296af9014a3f09e8263f46
Preston Mueller <preston.mueller@techshido.com>
2012-02-20 00:26:15 -0500

    Intial commmitttt

---
+++ Shitter.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  'Main shitter index'
+end
+
+get '/login' do
+  'Login to your shitter account'
+end
+
+get '/register' do
+  'Register a new shitter account'
+end
+
+get '/search' do
+  'This is the search page'
+end
